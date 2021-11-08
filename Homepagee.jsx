315d5066da38947077bd49dcd93747e3a1a7f6e9
adityajoshi721 <62933136+adityajoshi721@users.noreply.github.com>
2021-11-08 15:01:02 +0530

    Add files via upload

---
+++ Homepagee.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import millify from 'millify';
+import {Typography, Row, Col, Statistic} from 'antd';
+import { Link } from 'react-router-dom';
+
+import  { useGetCryptosQuery } from '../services/cryptoApi'
+import News from '../components/News'
+import Cryptocurrencies from '../components/Cryptocurrencies'
+const { Title } = Typography;
+
+const Homepagee = () => {
+
+    const { data, isFetching } = useGetCryptosQuery(12);
+    const globalStats = data?.data?.stats;
+
+    if(isFetching) 
+        return (<Title level = {1}>Loading...</Title>);
+
+    // console.log(data);
+    return (
+        <>
+            <Title level = {2} className = "heading">
+                Global Crypto Stats
+            </Title>
+            <Row>
+                <Col span = {12}>
+                    <Statistic title = "Total Crpto Currencies" value = {globalStats.total} />
+                </Col>
+                 <Col span = {12}>
+                    <Statistic title = "Total Exchanges" value = {millify(globalStats.totalExchanges)} />
+                </Col> 
+                <Col span = {12}>
+                    <Statistic title = "Total Market Cap" value = {millify(globalStats.totalMarketCap)} />
+                </Col> 
+                <Col span = {12}>
+                    <Statistic title = "Total 24h Volume" value = {millify(globalStats.total24hVolume)} />
+                </Col>
+                 <Col span = {12}>
+                    <Statistic title = "Total Markets" value = {millify(globalStats.totalMarkets)} />
+                </Col>
+            </Row>
+            <div className = "home-heading-container">
+                <Title level = {2} className = "home-title"> Top 10 Cryptocurrencies in the World </Title>
+                <Title level = {4} className = "show-more"> <Link to = '/cryptocurrencies'> Show More </Link></Title>
+            </div>
+            <Cryptocurrencies simplified/>
+            <div className = "home-heading-container">
+                <Title level = {2} className = "home-title"> Latest Crypto News </Title>
+                <Title level = {4} className = "show-more"> <Link to = '/news'> Show More </Link></Title>
+            </div>
+            <News simplified/>
+        </>
+    )
+}
+
+export default Homepagee
